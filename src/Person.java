cf850839f5629af36b8e19fcad0280b9f4d3b305
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2024-01-25 03:57:00 +0700

    class, object, method, field

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+class Person {
+    // Conthoh field (ada 3 field artinya) ntr masuk dan di print di page cpntohPerson
+    String name;
+    String address;
+    final String religion = "islam";
+
+    // Contoh method 
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + " My name is " + name);
+    }
+}
---
+++ src/contohPerson.java
@@ -0,0 +1,21 @@
+public class contohPerson {
+    public static void main(String[] args) {
+
+        // Contoh Object Java
+        var person1 = new Person();
+        person1.name = "Delf";
+        person1.address = "Jakarta";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.religion);
+
+        person1.sayHello("Andi Sutejo");
+
+
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+
+    }
+}
