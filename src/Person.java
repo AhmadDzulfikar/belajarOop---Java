71a5feabcaa4094fdc4f7a1a12136d2f2c950431
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2024-01-26 18:37:47 +0700

    shadow construction

--- src/Person.java
+++ src/Person.java
@@ -4,8 +4,22 @@ class Person {
     String address;
     final String religion = "islam";
 
+    // Membuat constructor
+    // Ada 3 constructor yang sama tetapi parameternya berbeda disebut constructor overloading
+Person (String name, String address) {
+    this.name = name;
+    this.address = address;
+}
+
+Person (String name) {
+    this(name, null); // Gunakan this() untuk memanggil constructor lain 
+}
+
+Person () {
+}
+
     // Contoh method 
-    void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + " My name is " + name);
+    void sayHello(String name) {
+        System.out.println("Hello " + name + " My name is " + this.name);
     }
 }
--- src/contohPerson.java
+++ src/contohPerson.java
@@ -2,20 +2,34 @@ public class contohPerson {
     public static void main(String[] args) {
 
         // Contoh Object Java
-        var person1 = new Person();
-        person1.name = "Delf";
-        person1.address = "Jakarta";
+        var person1 = new Person("Delf", "Condet"); // Contoh penggunaan constructor
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.religion);
 
-        person1.sayHello("Andi Sutejo");
+        person1.sayHello("Samiranadifa Azzahra");
 
+        System.out.println();
+        
+        Person person2 = new Person("Difa", "Cibubur");
+        System.out.println(person2.name);
+        System.out.println(person2.address);
+        System.out.println(person2.religion);
+        person2.sayHello("Ahmad Dzulfikar As Shavy");
+        // Person person3;
+        // person3 = new Person();
 
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
+        System.out.println();
+
+        Person person3 = new Person("Umay");
+        System.out.println(person3.name);
+
+        System.out.println();
+        
+        Person person4 = new Person();
+        person4.name = "Rahima";
+        System.out.println(person4.name);
 
     }
 }
